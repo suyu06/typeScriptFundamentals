f839dfecf85fc88b30fdd3ebb5a6bd60ac46d80f
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-01-29 22:38:24 -0500

    using template Strings

--- 02-variables/sample-types.js
+++ 02-variables/sample-types.js
@@ -6,6 +6,8 @@ console.log(found);
 console.log("The grade is " + grade);
 console.log("Hi " + firstName + " " + lastName);
 // let's break it!
-found = 0;
-grade = A;
-firstName = false;
+// found = 0; 
+// grade = A;
+// firstName = false;
+// use templates Strings
+console.log("Hi2 ".concat(firstName, "             ").concat(lastName));
